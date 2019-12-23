f64e5925f51ee4af1957803e8b5c3300c0ec4f66
Nino Filiu <nino.filiu@360learning.com>
2019-12-23 19:02:41 +0100

    [auth] db

---
+++ auth/db.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const { promisify } = require('util');
+
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+const dataPath = `${__dirname}/data.json`;
+
+const getData = async () => JSON.parse(await readFile(dataPath));
+const saveData = async (data) => writeFile(dataPath, JSON.stringify(data, null, 4));
+
+module.exports = {
+    async filter(predicate = () => true) {
+        const data = await getData();
+        return data.filter(predicate);
+    },
+    async push(item) {
+        const data = await getData();
+        data.push(item);
+        await saveData(data);
+    },
+};
