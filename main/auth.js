638446b0efcd467be026a184ae4b293528738b54
Nino Filiu <nino.filiu@360learning.com>
2019-12-26 19:15:33 +0100

    [main] use jwt

--- main/auth.js
+++ main/auth.js
@@ -1,11 +1,15 @@
+const tokens = require('../tokens');
+
 /** @type {import('express').RequestHandler} */
 module.exports = (req, res, next) => {
     const { token } = req.cookies;
 
-    if (token !== 'will be replaced by a jwt') {
+    try {
+        const username = tokens.verifiedSubject(token);
+        req.username = username;
+        return next();
+    } catch (e) {
         res.status(401);
         return res.send('Invalid authentication token');
     }
-
-    return next();
 };
